5d29164e24ea5fe970714ae302f26fb7bfc9f620
dgnnnnnnnnn <yelims@kakao.com>
2024-10-28 02:32:18 +0900

    241028 front(CertGallery edit)

--- yuza/client/src/certigallery/CertiGallery.js
+++ yuza/client/src/certigallery/CertiGallery.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { CiEdit, CiSaveDown2 } from "react-icons/ci";
@@ -58,19 +58,121 @@ const CertExplanation = styled.div`
   }
 `;
 
-// 여기부터 시험정보
+// 여기부터 자격증 정보 영역
 const DetailWrapper = styled.div`
   width: 100%;
-  min-height: 900px;
   background-color: #edf1f4;
   margin-top: 125px;
-  padding: 20px;
   position: relative;
+  z-index: 0; // z-index 추가
 `;
 
+// 네비게이션 바
+const DetailWrapperTitle = styled.div`
+  width: 100%;
+  height: 60px;
+  background-color: #f9f9f9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  position: sticky;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+  padding: 0;
+`;
+
+// 네비게이션 바의 버튼 속성
+const NavigationButton = styled.button`
+  padding: 5px 14px;
+  margin: 0 4px;
+  background-color: ${({ isActive }) => isActive ? '#ffffff' : '#f9f9f9'};
+  color: ${({ isActive }) => isActive ? '#000000' : '#8f8f8f'};
+  border: 1.5px solid ${({ isActive }) => isActive ? '#000000' : '#e0e0e0'};
+  border-radius: 4px;
+  font-size: 0.9rem;
+  cursor: pointer;
+  font-family: 'NanumSquareNeoLight', sans-serif;
+  font-weight: bold;
+  transition: all 0.1s ease;
+
+  // &:hover {
+  //   background-color: ${({ isActive }) => isActive ? '#1976D2' : '#f8f9fa'};
+  // }
+`;
+
+const ExamSchedule = styled.div`
+  width: 100%;
+  height: 900px;
+  background-color: #ffffff;
+  padding: 40px;
+`;
+
+const ScheduleContainer = styled.div`
+  width: 1100px;
+  margin: 0 auto;
+`;
+
+const TableTitle = styled.div`
+  font-family: 'NanumSquareNeoBold', sans-serif;
+  font-size: 1.6rem;
+  font-weight: bold;
+  color: #000000;
+  text-align: left;
+  margin-bottom: 10px;
+`;
+
+const TableSubTitle = styled.div`
+  font-size: 0.9rem;
+  color: #666666;
+  margin-bottom: 20px;
+  text-align: left;
+`;
+
+const Table = styled.table`
+  min-width: 1100px; // 테이블의 최소 너비 설정
+  width: 1100px; // 테이블의 고정 너비 설정
+  border-collapse: collapse;
+  table-layout: fixed;
+  margin: 0 auto; // 테이블 중앙 정렬
+`;
+
+const Th = styled.th`
+  padding: 15px 10px;
+  background-color: #dfe3ec;
+  color: #000000;
+  font-weight: bold;
+  text-align: center;
+  border: 1px solid #ccc;
+  font-size: 0.9rem;
+  word-break: keep-all;
+  line-height: 1.4;
+  vertical-align: middle;
+`;
+
+const Td = styled.td`
+  padding: 15px 10px;
+  background-color: #ffffff;
+  color: #111111;
+  border: 1px solid #ccc;
+  text-align: center;
+  font-size: 0.85rem;
+  line-height: 1.6;
+  vertical-align: middle;
+  white-space: pre-line;
+`;
+
+const ExamInfo = styled.div`
+  background-color: #ffffff;  // 배경색도 하얀색으로 변경
+  padding: 40px;  // 전체적인 padding 추가
+`;
+
+
+// 다운로드 버튼
 const DownloadButton = styled.button`
   position: absolute;
-  top: 20px;
+  top: -40px;
   right: 20px;
   background: none;
   border: none;
@@ -80,6 +182,7 @@ const DownloadButton = styled.button`
   color: black;
   font-family: 'NanumSquareNeoBold', sans-serif;
   font-size: 1rem;
+  z-index: 1;
 
   svg {
     margin-right: 5px;
@@ -177,7 +280,6 @@ const TransferIcon = styled.img`
 // 여기부터 유의사항
 const NoticeWrapper = styled.div`
   width: 100%;
-  height: 600px;
   background-color: #f2f2f2;
   display: flex;
   flex-direction: column;
@@ -208,6 +310,112 @@ const NoticeItem = styled.div`
 `;
 
 
+const ExamScheduleComponent = React.forwardRef(({ scheduleInfo }, ref) => {
+  if (!scheduleInfo) return null;
+
+
+  return (
+    <ExamSchedule ref={ref}>
+       <ScheduleContainer>
+      <TableTitle>{scheduleInfo.title}</TableTitle>
+      <TableSubTitle>{scheduleInfo.subtitle}</TableSubTitle>
+      <Table>
+        <thead>
+          <tr>
+            {scheduleInfo.headerData.map((header, index) => (
+              <Th key={index}>{header}</Th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {scheduleInfo.scheduleData.map((row, index) => (
+            <tr key={index}>
+              <Td>{row.term}</Td>
+              <Td>{row.writtenRegistration}</Td>
+              <Td>{row.writtenExam}</Td>
+              <Td>{row.writtenPassAnnouncement}</Td>
+              <Td>{row.practicalRegistration}</Td>
+              <Td>{row.practicalExam}</Td>
+              <Td>{row.finalPassAnnouncement}</Td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      </ScheduleContainer>
+    </ExamSchedule>
+  );
+});
+
+const ExamInfoComponent = React.forwardRef(({ examInfo }, ref) => {
+  if (!examInfo) return null;
+
+  return (
+    <ExamInfo ref={ref}>
+      <ScheduleContainer>
+        <TableTitle>{examInfo.title}</TableTitle>
+        <Table>
+          <thead>
+          <tr>
+              {examInfo.headerData.map((header, index) => (
+                <Th key={index} style={{ 
+                  width: header === "구분" ? "10%" :       // 구분 열
+                         header === "시험과목" ? "40%" :   // 시험과목 열
+                         "16.66%"                          // 나머지 열들
+                }}>
+                  {header}
+                </Th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <Td rowSpan={5} style={{ backgroundColor: '#f8f9fa' }}>필기</Td>
+              <Td>1. 소프트웨어 설계</Td>
+              <Td rowSpan={5}>객관식 4지 택일형{"\n"}과목당 20문항{"\n"}(과목당 30분)</Td>
+              <Td rowSpan={5}>100점을 만점으로 하여{"\n"}과목당 40점 이상,{"\n"}전 과목 평균 60점 이상</Td>
+              <Td rowSpan={5}>19,400원</Td>
+            </tr>
+            {[
+              "2. 소프트웨어 개발",
+              "3. 데이터베이스 구축",
+              "4. 프로그래밍 언어 활용",
+              "5. 정보시스템 구축 관리"
+            ].map((subject, index) => (
+              <tr key={index}>
+                <Td>{subject}</Td>
+              </tr>
+            ))}
+            <tr>
+              <Td rowSpan={12} style={{ backgroundColor: '#f8f9fa' }}>실기</Td>
+              <Td>1. 요구사항 확인</Td>
+              <Td rowSpan={12}>필답형(2시간 30분)</Td>
+              <Td rowSpan={12}>100점을 만점으로 하여 60점 이상</Td>
+              <Td rowSpan={12}>22,600원</Td>
+            </tr>
+            {[
+              "2. 데이터입출력 구현",
+              "3. 통합 구현",
+              "4. 서버프로그램 구현",
+              "5. 인터페이스 구현",
+              "6. 화면 설계",
+              "7. 애플리케이션테스트 관리",
+              "8. SQL 응용",
+              "9. 소프트웨어 개발 보안 구축",
+              "10. 프로그래밍 언어 활용",
+              "11. 응용SW기초 기술 활용",
+              "12. 제품 소프트웨어 패키징"
+            ].map((subject, index) => (
+              <tr key={index}>
+                <Td>{subject}</Td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </ScheduleContainer>
+    </ExamInfo>
+  );
+});
+
 const createMarkup = (html) => {
   return { __html: DOMPurify.sanitize(html) };
 }
@@ -216,6 +424,71 @@ function CertiGallery() {
   const navigate = useNavigate();
   const { certId } = useParams();
   const [certData, setCertData] = useState(null);
+  const [activeSection, setActiveSection] = useState('schedule');
+  const [isNavVisible, setIsNavVisible] = useState(false);
+  // const [isNavFixed, setIsNavFixed] = useState(false);
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  const scheduleRef = useRef(null);
+  const infoRef = useRef(null);
+  const trendRef = useRef(null);
+  const detailWrapperRef = useRef(null);
+  const titleWrapperRef = useRef(null);
+
+
+// Simplify useEffect
+useEffect(() => {
+  const handleScroll = () => {
+    if (isScrolling) return;
+
+    const schedule = scheduleRef.current?.getBoundingClientRect();
+    const info = infoRef.current?.getBoundingClientRect();
+
+    const offset = 60;
+
+    if (info && info.top <= offset && info.bottom >= offset) {
+      setActiveSection('info');
+    } else if (schedule && schedule.top <= offset && schedule.bottom >= offset) {
+      setActiveSection('schedule');
+    }
+  };
+
+  window.addEventListener('scroll', handleScroll);
+  return () => window.removeEventListener('scroll', handleScroll);
+}, [isScrolling]);
+
+const scrollToSection = (sectionId) => {
+  let targetRef;
+
+  switch (sectionId) {
+    case 'schedule':
+      targetRef = scheduleRef;
+      break;
+    case 'info':
+      targetRef = infoRef;
+      break;
+    default:
+      return;
+  }
+
+  if (targetRef.current) {
+    const yOffset = -60;
+    const element = targetRef.current;
+    const y = element.getBoundingClientRect().top + window.scrollY + yOffset;
+
+    setIsScrolling(true);
+    setActiveSection(sectionId);
+
+    window.scrollTo({
+      top: y,
+      behavior: 'smooth',
+    });
+
+    setTimeout(() => {
+      setIsScrolling(false);
+    }, 500);
+  }
+};
 
   useEffect(() => {
     // 여기서 자격증 데이터를 가져옵니다.
@@ -227,53 +500,94 @@ function CertiGallery() {
     // 예시 데이터
     const data = {
       1: {
-        name: "기계조립산업기사",
+        name: "정보처리기사",
         management: "한국산업인력공단",
         Explanation: "공작기계인 범용선반, 범용밀링, 탁상드릴, 연삭기와 수공구 등을 사용하여<br/><strong>재료를 가공하는 것을 평가</strong>하는 시험입니다.",
-        written: {
-          registration: "24.06.18~24.06.21",
-          exam: "24.07.05~24.07.27",
-          details: [
-            "객관식 4지 택일형",
-            "과목당 20문항(과목당 30분)",
-            "과락 40점, 전 과목 평균 60점 이상"
+        scheduleInfo: {
+          title: "시험일정",
+          subtitle: "정보처리기사(원서접수시간은 원서접수 첫날 10:00부터 마지막 날 18:00까지 임)",
+          headerData: [
+            "구분",
+            "필기원서접수(인터넷)(휴일제외)",
+            "필기시험",
+            "필기합격(예정자)발표",
+            "실기원서접수(휴일제외)",
+            "실기시험",
+            "최종합격자 발표일"
           ],
-          resultDate: "2024.08.07"
+          scheduleData: [
+            {
+              term: "2024년 정기 기사 1회",
+              writtenRegistration: "2024.01.23\n~\n2024.01.26\n[빈자리접수 : \n2024.02.09\n~\n2024.02.10]",
+              writtenExam: "2024.02.15\n~\n2024.03.07",
+              writtenPassAnnouncement: "2024.03.13",
+              practicalRegistration: "2024.03.26\n~\n2024.03.29\n빈자리접수 :\n2024.04.21\n~\n2024.04.22",
+              practicalExam: "2024.04.27\n~\n2024.05.17",
+              finalPassAnnouncement: "2024.06.18"
+            },
+            {
+              term: "2024년 정기 기사 2회",
+              writtenRegistration: "2024.04.16\n~\n2024.04.19\n[빈자리접수 : \n2024.05.03\n~\n2024.05.04]",
+              writtenExam: "2024.05.09\n~\n2024.05.28",
+              writtenPassAnnouncement: "2024.06.05",
+              practicalRegistration: "2024.06.25\n~\n2024.06.28\n빈자리접수 :\n2024.07.22\n~\n2024.07.23",
+              practicalExam: "2024.07.28\n~\n2024.08.14",
+              finalPassAnnouncement: "2024.09.10"
+            },
+            {
+              term: "2024년 정기 기사 3회",
+              writtenRegistration: "2024.06.18\n~\n2024.06.21\n[빈자리접수 : \n2024.06.29\n~\n2024.06.30]",
+              writtenExam: "2024.07.05\n~\n2024.07.27",
+              writtenPassAnnouncement: "2024.08.07",
+              practicalRegistration: "2024.09.10\n~\n2024.09.13\n빈자리접수 :\n2024.10.14\n~\n2024.10.14",
+              practicalExam: "2024.10.19\n~\n2024.11.08",
+              finalPassAnnouncement: "2024.12.11"
+            }
+          ]
         },
-        practical: {
-          registration: "24.09.10~24.09.13",
-          exam: "24.10.19~24.11.08",
-          details: [
-            "작업형(4시간 50분, 100점)",
-            "60점 이상"
-          ],
-          resultDate: "2024.12.11"
-        },
-        downloadFile: "기계조립산업기사(2022.1.1～2026.12.31).hwp"
+        examInfo: {
+          title: "시험과목 및 합격기준",
+          headerData: ["구분", "시험과목", "검정방법", "합격기준", "응시료"],
+          subjectData: {
+            written: {
+              subjects: [
+                "1. 소프트웨어 설계",
+                "2. 소프트웨어 개발",
+                "3. 데이터베이스 구축",
+                "4. 프로그래밍 언어 활용",
+                "5. 정보시스템 구축 관리"
+              ],
+              method: "객관식 4지 택일형\n과목당 20문항\n(과목당 30분)",
+              criteria: "100점을 만점으로 하여\n과목당 40점 이상,\n전 과목 평균 60점 이상",
+              fee: "19,400원"
+            },
+            practical: {
+              subjects: [
+                "1. 요구사항 확인",
+                "2. 데이터입출력 구현",
+                "3. 통합 구현",
+                "4. 서버프로그램 구현",
+                "5. 인터페이스 구현",
+                "6. 화면 설계",
+                "7. 애플리케이션테스트 관리",
+                "8. SQL 응용",
+                "9. 소프트웨어 개발 보안 구축",
+                "10. 프로그래밍 언어 활용",
+                "11. 응용SW기초 기술 활용",
+                "12. 제품 소프트웨어 패키징"
+              ],
+              method: "필답형(2시간 30분)",
+              criteria: "100점을 만점으로 하여 60점 이상",
+              fee: "22,600원"
+            }
+          }
+        }
       },
-      2: {
-        name: "컴퓨터응용가공산업기사",
-        management: "한국산업인력공단",
-        Explanation: "컴퓨터응용가공에 대한 설명...",
-        written: {
-          registration: "날짜 미정",
-          exam: "날짜 미정"
-        },
-        practical: {
-          registration: "날짜 미정",
-          exam: "날짜 미정"
-        },
-        downloadFile: "컴퓨터응용가공산업기사(2022.1.1～2026.12.31).hwp"
-      },
-      3: { name: "공조냉동기계산업기사" },
-      4: { name: "가스산업기사" }
+      // ... 다른 자격증 데이터
     };
     setCertData(data[id]);
   };
 
-  if (!certData) {
-    return <div>Loading...</div>;
-  }
 
   // 출제기준 다운로드 함수
   const handleDownload = () => {
@@ -304,103 +618,58 @@ function CertiGallery() {
     navigate('/board/cert', { state: { selectedCertType: certData.name } });
   };
 
+
+  if (!certData) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Container>
-      {/* 자격증 제목 및 간단한 설명 */}
-      <TitleWrapper>
+      <TitleWrapper ref={titleWrapperRef}>
         <CiEdit size={45} />
         <CertName>{certData.name}</CertName>
         <CertManagement>(시행처 : {certData.management})</CertManagement>
         <CertExplanation dangerouslySetInnerHTML={createMarkup(certData.Explanation)} />
       </TitleWrapper>
-      {/* 말풍선에 시험 일정, 수수료 등 상세한 설명 */}
-      <DetailWrapper>
+
+      <DetailWrapper ref={detailWrapperRef}>
+      <DetailWrapperTitle>
+          <NavigationButton
+            isActive={activeSection === 'schedule'}
+            onClick={() => scrollToSection('schedule')}
+          >
+            시험일정
+          </NavigationButton>
+          <NavigationButton
+            isActive={activeSection === 'info'}
+            onClick={() => scrollToSection('info')}
+          >
+            시험과목 및 합격기준
+          </NavigationButton>
+          {/* <NavigationButton
+            isActive={activeSection === 'trend'}
+            onClick={() => scrollToSection('trend')}
+          >
+            출제경향
+          </NavigationButton> */}
+        </DetailWrapperTitle>
         <DownloadButton onClick={handleDownload}>
           <CiSaveDown2 size={24} />
           출제기준 다운로드
         </DownloadButton>
-        <BubbleName>시험 정보</BubbleName>
-        <BubbleContainer>
-          <BubbleRow>
-            <BubbleWrapper>
-              <BubbleImage src={InfoBubbleDetail} alt="InfoBubbleDetail" />
-              <BubbleContent>
-                <BubbleTitle>필기 시험 방식</BubbleTitle>
-                <BubbleText>
-                  {certData.written?.details.map((detail, index) => (
-                    <React.Fragment key={index}>
-                      {detail}
-                      {index < certData.written.details.length - 1 && <br />}
-                    </React.Fragment>
-                  ))}
-                </BubbleText>
-              </BubbleContent>
-            </BubbleWrapper>
-            <BubbleWrapper>
-              <BubbleImage src={InfoBubbleDetail} alt="InfoBubbleDetail" />
-              <BubbleContent>
-                <BubbleTitle>실기 시험 방식</BubbleTitle>
-                <BubbleText>
-                  {certData.practical?.details.map((detail, index) => (
-                    <React.Fragment key={index}>
-                      {detail}
-                      {index < certData.practical.details.length - 1 && <br />}
-                    </React.Fragment>
-                  ))}
-                </BubbleText>
-              </BubbleContent>
-            </BubbleWrapper>
-          </BubbleRow>
-          <BubbleRow>
-            <BubbleWrapper>
-              <BubbleImage src={InfoBubble} alt="Info Bubble" />
-              <BubbleContent>
-                <BubbleTitle>일정</BubbleTitle>
-                <BubbleText>
-                  접수 {certData.written?.registration || "날짜 미정"}<br />
-                  시험 {certData.written?.exam || "날짜 미정"}
-                </BubbleText>
-              </BubbleContent>
-            </BubbleWrapper>
-            <BubbleWrapper>
-              <BubbleImage src={InfoBubble} alt="Info Bubble" />
-              <BubbleContent>
-                <BubbleTitle>일정</BubbleTitle>
-                <BubbleText>
-                  접수 {certData.practical?.registration || "날짜 미정"}<br />
-                  시험 {certData.practical?.exam || "날짜 미정"}
-                </BubbleText>
-              </BubbleContent>
-            </BubbleWrapper>
-          </BubbleRow>
-          <BubbleRow>
-            <BubbleWrapper>
-              <BubbleImage src={InfoBubble} alt="Info Bubble" />
-              <BubbleContent>
-                <BubbleTitle>필기 발표일</BubbleTitle>
-                <BubbleText>
-                  {certData.written?.resultDate || "날짜 미정"}
-                </BubbleText>
-              </BubbleContent>
-            </BubbleWrapper>
-            <BubbleWrapper>
-              <BubbleImage src={InfoBubble} alt="Info Bubble" />
-              <BubbleContent>
-                <BubbleTitle>실기 발표일</BubbleTitle>
-                <BubbleText>
-                  {certData.practical?.resultDate || "날짜 미정"}
-                </BubbleText>
-              </BubbleContent>
-            </BubbleWrapper>
-          </BubbleRow>
-        </BubbleContainer>
+
+        <ExamScheduleComponent ref={scheduleRef} scheduleInfo={certData.scheduleInfo} />  {/* 시험일정 */}
+        <ExamInfoComponent ref={infoRef} examInfo={certData.examInfo} />                  {/* 시험정보 */}
+        {/* <ExamTrend ref={trendRef} /> */}
       </DetailWrapper>
+
       <TransferSection>
         <TransferButton onClick={handleTransfer}>
           <TransferIcon src={CertGalleryTransfer} alt="Transfer Icon" />
           {certData.name} 공부하러 이동하기!
         </TransferButton>
       </TransferSection>
+
       <NoticeWrapper>
         <NoticeTitle>유의사항</NoticeTitle>
         <NoticeContent>
